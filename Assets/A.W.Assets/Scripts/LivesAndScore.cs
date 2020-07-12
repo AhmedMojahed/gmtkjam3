471f32776c8dc905dadbbde46e3f084db3e04910
Amjad Mohamed <amjadmohamed55@gmail.com>
2020-07-12 20:16:00 +0200

    Added BG

--- Assets/A.W.Assets/Scripts/LivesAndScore.cs
+++ Assets/A.W.Assets/Scripts/LivesAndScore.cs
@@ -5,15 +5,27 @@ using UnityEngine.UI;
 using TMPro;
 public class LivesAndScore : MonoBehaviour
 {
-    [SerializeField] int score = 200, lives = 3;
+    [SerializeField] int score = 200; 
+    [HideInInspector]public int lives = 3;
     [SerializeField] TextMeshProUGUI livesText;
 
+    public static LivesAndScore LAS;
     // Start is called before the first frame update
     void Start()
     {
+        
+
+        if(!LAS)
+        {
+            LAS = this;
+        }
+    }
+
+    private void Update()
+    {
         UpdateText();
     }
-    
+
     private void UpdateText()
     {
         //scoreText.text = score.ToString();
---
+++ Assets/Amjad/Dmg.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Dmg : MonoBehaviour
+{
+    public GameObject SpawnPosition;
+    public Slider slider;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "Acid")
+        {
+            Debug.Log("CColluided");
+            LivesAndScore.LAS.TakeLife();
+        }
+    }
+
+    private void Update()
+    {
+        if(this.transform.position.y < -8)
+        {
+            LivesAndScore.LAS.TakeLife();
+        }
+
+        if(this.transform.position.y < -8 && LivesAndScore.LAS.lives > 0)
+        {
+            this.transform.position = SpawnPosition.transform.position;
+        }
+
+        slider.value += 0.05f;
+
+        /*if(slider.value >= 50)
+        {
+            Slider. Color.Lerp(Color.yellow, Color.red, 0.8f);
+        }*/
+    }
+}
